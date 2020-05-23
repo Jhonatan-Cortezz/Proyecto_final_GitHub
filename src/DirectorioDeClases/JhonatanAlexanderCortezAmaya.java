ff54bbe38a5bf7f883cfb3c2001b814f98b3609a
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-05-23 08:37:47 -0600

    Agregue el directorio que contiene las clases de cada integrante

---
+++ src/DirectorioDeClases/JhonatanAlexanderCortezAmaya.java
@@ -0,0 +1,14 @@
+
+package DirectorioDeClases;
+
+/**
+ *
+ * @author JHONATAN CORTEZ
+ */
+public class JhonatanAlexanderCortezAmaya {
+    public static void main(String[] args) {
+        
+        String miPrimerMensaje = "Hola mundo";
+        System.out.printf("La variable miPrimerMensaje contiene %s%n ", miPrimerMensaje);
+    }
+}
