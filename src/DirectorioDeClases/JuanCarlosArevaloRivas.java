dfdbd6acf2ddd7728acd535054d1760ffc410ec7
Juan1035 <61745967+Juan1035@users.noreply.github.com>
2020-05-23 16:54:40 -0600

    Nuevo codigo agregado

---
+++ src/DirectorioDeClases/JuanCarlosArevaloRivas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DirectorioDeClases;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Juan Carlos Arevalo
+ */
+public class JuanCarlosArevaloRivas {
+        public static void main(String[] args) {
+         
+        //Nos aparece un cuadro de dialogo
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu nombre");
+        String nombre=sc.nextLine();
+  
+        System.out.println("Bienvenido "+nombre);
+    }
+     
+}
+
