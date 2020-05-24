b0ef8f5a53f27fb5f7a11a16d24535c39f963a8e
CrisMrtz01 <kooper8273@gmail.com>
2020-05-23 18:20:01 -0600

    Hago uso del IDE Netbeans para la siguiente tarea

---
+++ src/DirectorioDeClases/CristianArnoldoMartinezMartinez.java
@@ -0,0 +1,14 @@
+
+package DirectorioDeClases;
+
+/**
+ *
+ * @author Cristian Arnoldo Martinez Martinez
+ */
+public class CristianArnoldoMartinezMartinez {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HOLA MUNDO");
+        }
+    }
+}
